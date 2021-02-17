fb086e6476a066aaf20df317a1772c64122f1793
Aleksander <71639465+alpavlinov04@users.noreply.github.com>
2021-02-17 11:54:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace road
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Road firstRoad = new Road();
+            Road secondRoad = new Road("Highway", 140, 1550, 4, "E79");
+            Console.WriteLine(firstRoad);
+            Console.WriteLine(secondRoad);
+        }
+    }
+}
---
+++ Road.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace road
+{
+    class Road
+    {
+        public string Type;
+        public int Speed;
+        public int Sails;
+        public int Length;
+        public string Name;
+
+        public Road(string T, int s, int sa, int l, string n)
+        {
+            this.Type = T;
+            this.Speed = s;
+            this.Sails = sa;
+            this.Length = l;
+            this.Name = n;
+        }
+        public Road()
+        {
+            this.Type = "Highway";
+            this.Speed = 140;
+            this.Sails = 418;
+            this.Length = 4;
+            this.Name = "Hemus";
+
+        }
+        public override string ToString()
+        {
+            return "The type is: " + " " + this.Type + "the speed: " +" " + this.Speed + "the sails: " +" " + this.Sails + "the lenght: " + " " + this.Length + "the name of road is:" + this.Name;
+        }
+    }
+    
+}
